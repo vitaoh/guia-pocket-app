bab3990bbb40ef3b934a3503577a73d58cfd9542
victorherculini <herculinvictorr@gmail.com>
2025-10-13 16:42:32 -0300

    adicionando modo escuro e ptbr

--- app/src/main/java/com/example/guia_pocket_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/guia_pocket_app/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.guia_pocket_app
+package com.example.guia_pocket_app.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.guia_pocket_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
